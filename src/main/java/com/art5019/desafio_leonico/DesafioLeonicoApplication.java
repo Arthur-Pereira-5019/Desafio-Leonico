edb5c583cae88f1a780b345ca046faaf673270f0
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-09-02 18:41:02 -0300

    First version of the repository, started to create the html of the file

---
+++ src/main/java/com/art5019/desafio_leonico/DesafioLeonicoApplication.java
@@ -0,0 +1,13 @@
+package com.art5019.desafio_leonico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLeonicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLeonicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/art5019/desafio_leonico/DesafioLeonicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art5019.desafio_leonico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLeonicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
